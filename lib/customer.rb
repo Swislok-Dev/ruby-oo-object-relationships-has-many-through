8af85f1d12dc9c05751b010d5b111f4d425e48bf
cmarker <cmarker4@gmail.com>
2020-11-21 08:29:33 -0600

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,34 @@
+require "pry"
+
 class Customer
-  
+    attr_accessor :name, :age, :waiter, :total, :tip
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        arr = []
+        Meal.all.each { |meal| arr << meal if meal.customer == self }
+        arr
+    end
+
+    def waiters
+        arr = []
+        Meal.all.each { |meal| arr << meal.waiter if meal.customer == self }
+        arr
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,42 @@
-class Waiter
 
+class Waiter
+    attr_accessor :name, :years, :customer, :total, :tip
+
+    @@all = []
+
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+    
+    def meals
+        arr = []
+        Meal.all.each do |meal|
+            if meal.waiter == self
+                arr << meal
+            end
+        end
+        arr
+    end
+
+    def best_tipper
+        best_tip = nil
+        tip = 0
+        Meal.all.each do |meal|
+            if meal.tip > tip
+                best_tip = meal.customer
+                tip = meal.tip
+            end
+        end
+        best_tip
+    end
 end
